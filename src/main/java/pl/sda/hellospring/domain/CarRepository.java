58d6b7eb54793a9f3ff719287b4afa883d23c963
Tomek <tomekpl77@gmail.com>
2020-09-27 09:23:11 +0200

    [change] - CarConverter, Day 1 closed.

--- src/main/java/pl/sda/hellospring/domain/CarRepository.java
+++ src/main/java/pl/sda/hellospring/domain/CarRepository.java
@@ -2,6 +2,9 @@ package pl.sda.hellospring.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CarRepository extends CrudRepository<Car, Long> {
 
+    Optional<Car> findByBrandAndColor(String brand, String color);
 }
